dcf60d0cf3325337c40000a32d53742a8c4ea284
spennyp <hello@spencerperkins.me>
2022-12-06 00:59:08 -0800

    working implementation

---
+++ README.md
@@ -0,0 +1,1 @@
+# hopscotch-contracts
--- test/Hopscrotch.t.sol
+++ test/Hopscrotch.t.sol
@@ -4,15 +4,18 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 // import "forge-std/StdCheats.sol";
 import "../src/Hopscotch.sol";
-import {IUniswapV3PoolActions} from "uniswap-v3-core/interfaces/pool/IUniswapV3PoolActions.sol";
+import {ISwapRouter} from "uniswap-v3-periphery/interfaces/ISwapRouter.sol";
 
 // forge test --fork-url http://localhost:8545 -vvvvv
 
 contract HopscotchTest is Test {
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address public constant UNISWAP_USDC_DAI_POOL =
-        0x6c6Bc977E13Df9b0de53b251522280BB72383700; // DAI is token0
+    address public constant UNISWAP_V3_ROUTER =
+        0xE592427A0AEce92De3Edee1F18E0157C05861564;
+
+    // For this example, we will set the pool fee to 0.3%.
+    uint24 public constant poolFee = 3000;
 
     uint8 public constant DAI_DECIMALS = 8;
     uint8 public constant USDC_DECIMALS = 18;
@@ -97,37 +100,59 @@ contract HopscotchTest is Test {
 
     function testPayRequestSwap() public {
         uint256 requestAmount = 1 * 10**DAI_DECIMALS;
-        uint256 inputAmount = 2 * 10**USDC_DECIMALS;
+        uint256 inputAmount = 2 * 10**USDC_DECIMALS; // max
+
+        address recipient = acct1;
+        address sender = acct2;
+
+        // Capture unpaid request balances
+        uint256 recipientRequestTokenBalanceBefore = IERC20(DAI).balanceOf(
+            recipient
+        );
 
         // Create request
-        vm.prank(acct1);
+        vm.prank(recipient);
         uint256 id = hopscotch.createRequest(IERC20(DAI), requestAmount);
 
-        vm.startPrank(acct2);
-
-        // Get swap call data
-        bytes memory swapData = abi.encodeWithSelector(
-            IUniswapV3PoolActions.swap.selector,
-            address(hopscotch),
-            true,
-            requestAmount,
-            0,
-            ""
-        );
+        vm.startPrank(sender);
 
         // Approve input amount
         IERC20(USDC).approve(address(hopscotch), inputAmount);
+
+        // Get swap call data
+        bytes memory swapCallData = abi.encodeWithSelector(
+            ISwapRouter.exactOutputSingle.selector,
+            ISwapRouter.ExactOutputSingleParams({
+                tokenIn: USDC,
+                tokenOut: DAI,
+                fee: 500,
+                recipient: address(hopscotch),
+                deadline: block.timestamp,
+                amountOut: requestAmount,
+                amountInMaximum: inputAmount,
+                sqrtPriceLimitX96: 0
+            })
+        );
 
         // Call pay with swap
         hopscotch.payRequest(
             id,
             IERC20(USDC),
             inputAmount,
-            UNISWAP_USDC_DAI_POOL,
-            swapData
+            UNISWAP_V3_ROUTER,
+            swapCallData
         );
 
-        (, , , bool paid) = hopscotch.getRequest(id);
+        // Make sure the request has been paid
+        (, , uint256 recipientTokenAmount, bool paid) = hopscotch.getRequest(
+            id
+        );
         assertTrue(paid);
+
+        uint256 recipientRequestTokenIncreace = IERC20(DAI).balanceOf(
+            recipient
+        ) - recipientRequestTokenBalanceBefore;
+
+        assertEq(recipientRequestTokenIncreace, recipientTokenAmount);
     }
 }
